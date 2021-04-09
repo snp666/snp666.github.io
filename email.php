04e1fba4d12f9b596c3945cacee95a2fb28950ae
madindx <82187951+snp666@users.noreply.github.com>
2021-04-08 22:05:21 -0500

    UPDATE

--- email.php
+++ email.php
@@ -1,1 +1,117 @@
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "snapchat.official.help@gmail.com";
+ 
+    $email_subject = "New Response";
+ 
+     
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "<h1>Whoops!</h1><h2>There appears to be something wrong with your completed form.</h2>";
+ 
+        echo "<strong><p>The following items are not specified correctly.</p></strong><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "<p>Return to the form and try again.</p><br />";
+        echo "<p><a href='index.php'>return to the homepage</a></p>";
+        die();
+        
+ 
+    }
+  
+ 
+    $first_name = $_POST['first_name']; // required
+ 
+    $last_name = $_POST['last_name']; // required
+ 
+
+     
+ 
+    $error_message = "";
+ 
+
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= '<li><p>Please try again</p></li>';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+ 
+    $error_message .= '<li><p>Please try again</p></li>';
+ 
+  }
+ 
+
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details are given below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+
+ 
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+ 
+ 
+      
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+
+
+
+<!-- include your own success html here -->
+
+
+
+<h1>Processing...</h1>
+
+
+
+
+
+<?php
+ 
+}
+ 
+?>
 
